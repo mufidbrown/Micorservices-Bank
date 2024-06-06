9a023c2bc8b29458df44040641605b348d93dce5
mufidbrown <ilhammufid67@gmail.com>
2024-06-07 02:35:45 +0700

    add api-gateway

---
+++ internet-banking-api-gateway/src/main/java/com/mufid/finance/internet_banking_api_gateway/InternetBankingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mufid.finance.internet_banking_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApiGatewayApplication.class, args);
+	}
+
+}
